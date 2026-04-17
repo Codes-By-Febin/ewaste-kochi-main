7b1866cba1c06d1fc72385adc03b8fac21307450
Developer <dev@ewastekochi.com>
2026-04-18 00:08:06 +0530

    Add remaining pages: about, faq, 404, privacy; add robots.txt and favicon

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,76 +1,49 @@
-const {
-    defineConfig,
-    globalIgnores,
-} = require("eslint/config");
+const tseslint = require('@typescript-eslint/eslint-plugin');
+const tseslintParser = require('@typescript-eslint/parser');
+const globals = require('globals');
 
-const globals = require("globals");
-const parser = require("astro-eslint-parser");
-const tsParser = require("@typescript-eslint/parser");
-const js = require("@eslint/js");
-
-const {
-    FlatCompat,
-} = require("@eslint/eslintrc");
-
-const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
-});
-
-module.exports = defineConfig([{
-    extends: compat.extends("eslint:recommended"),
-
-    languageOptions: {
-        ecmaVersion: 2022,
-        sourceType: "module",
-        parserOptions: {},
-
-        globals: {
-            ...globals.browser,
-            ...globals.node,
-            console: "readonly",
-            process: "readonly",
-            Buffer: "readonly",
-            Map: "readonly",
-            Set: "readonly",
-            IntersectionObserver: "readonly",
-        },
+module.exports = [
+  {
+    files: ['**/*.{js,mjs,cjs,ts,tsx,jsx}'],
+    plugins: {
+      '@typescript-eslint': tseslint,
     },
-
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        Buffer: 'readonly',
+        Map: 'readonly',
+        Set: 'readonly',
+        IntersectionObserver: 'readonly',
+      },
+    },
+  },
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    plugins: {
+      '@typescript-eslint': tseslint,
+    },
+    languageOptions: {
+      parser: tseslintParser,
+      parserOptions: {
+        ecmaVersion: 2022,
+        sourceType: 'module',
+      },
+    },
     rules: {
-        "no-unused-vars": "warn",
-        "no-undef": "off",
+      'no-unused-vars': 'warn',
+      'no-undef': 'off',
     },
-}, {
-    files: ["**/*.astro"],
-    extends: compat.extends("plugin:astro/recommended"),
-
-    languageOptions: {
-        parser: parser,
-
-        parserOptions: {
-            parser: "@typescript-eslint/parser",
-            extraFileExtensions: [".astro"],
-        },
-
-        globals: {
-            ...globals.browser,
-            ...globals.node,
-        },
-    },
-}, {
-    files: ["**/*.ts", "**/*.tsx"],
-
-    languageOptions: {
-        parser: tsParser,
-        ecmaVersion: 2022,
-        sourceType: "module",
-        parserOptions: {},
-
-        globals: {
-            ...globals.browser,
-            ...globals.node,
-        },
-    },
-}, globalIgnores(["**/dist/", "**/node_modules/", "**/*.config.js", "**/*.config.mjs"])]);
+  },
+  {
+    ignores: [
+      'dist/',
+      'node_modules/',
+      '*.config.js',
+      '*.config.mjs',
+    ],
+  },
+];
